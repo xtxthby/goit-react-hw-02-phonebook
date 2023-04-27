54788ec2051be669e950822052ef4626b13ea5be
Yurii <116266510+xtxthby@users.noreply.github.com>
2023-04-27 23:16:48 +0300

    add files

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,85 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+import React, { Component } from 'react';
+import { ContactForm } from './ContactForm/ContactForm';
+import { ContactList } from './ContactList/ContactList';
+import { Header } from './Header/Header';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import Filter from './Filter/Filter';
+import { Section } from './Section/Section';
+import initialContacts from './contacts.json';
+
+
+// стилі для ToastContainer
+const notifyOptions = {
+  position: 'bottom-left',
+  autoClose: 5000,
+  hideProgressBar: false,
+  closeOnClick: true,
+  pauseOnHover: true,
+  draggable: true,
+  progress: undefined,
+  theme: 'colored',
+};
+
+export class App extends Component{
+
+  state = {
+    contacts: initialContacts,
+    filter: '',
+  }
+  // додавання нового контакту с забороною додачі однакового
+  addContact = newContact => {
+    this.state.contacts.filter(
+      contact =>
+        contact.name.toLowerCase().trim() ===
+        newContact.name.toLowerCase().trim() ||
+        contact.number.trim() === newContact.number.trim()
+    ).length
+      ? toast.error(`${newContact.name}: is already in contacts`, notifyOptions)
+      : this.setState(prevState => {
+        return {
+          contacts: [newContact, ...prevState.contacts],
+        };
+      });
+  };
+
+  deleteContact = contactId => {
+    this.setState(prevState => {
+      return {
+        contacts: prevState.contacts.filter(
+          contact => contact.id !== contactId
+        ),
+      };
+    });
+  };
+
+  changeFilter = e => {
+    this.setState({ filter: e.currentTarget.value.toLowerCase() });
+  };
+
+  getVisibleContacts = () => {
+    //  деструкторізація
+    const { filter, contacts } = this.state;
+    // приводимо до регістру
+    const normalizedFilter = filter.toLowerCase();
+    return contacts.filter(contact =>
+      // порівняння
+      contact.name.toLowerCase().includes(normalizedFilter)
+    );
+  };
+  render() {
+    const { filter } = this.state;
+    const visibleContacts = this.getVisibleContacts();
+    return (
+      <>
+        <Section title= "Phonebook">
+          <ContactForm onAddContact={this.addContact} />
+          <Header title="Contacts" />
+          <Filter value={filter} onChange={this.changeFilter} />
+          <ContactList contacts={visibleContacts} onDelete={this.deleteContact} />
+        </Section>
+        <ToastContainer />
+      </>
+    );
+  }
 };
